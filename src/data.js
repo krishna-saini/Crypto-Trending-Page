f9844a69a4e8c1ebe63b9f81f80e64872c6d82c9
aditya <adityakadali00@gmail.com>
2022-12-08 20:40:10 +0530

    added data

---
+++ src/data.js
@@ -0,0 +1,127 @@
+export const data = [
+  {
+    icon: "https://imgur.com/67isT37.png",
+    name: "Bitcoin",
+    ticker: "BTC",
+    price: "31812.80",
+    changePercentage: "+10",
+    TVL: "60000",
+    popularPairs: [
+      {
+        name: "Solana",
+        ticker: "SOL",
+        icon: "https://imgur.com/eAI0rkz.png",
+      },
+      {
+        name: "Ethereum",
+        ticker: "ETH",
+        icon: "https://imgur.com/Su3We8P.png",
+      },
+      {
+        name: "Binance USD",
+        ticker: "BUSD",
+        icon: "https://imgur.com/LCEltEU.png",
+      },
+    ],
+  },
+  {
+    icon: "https://imgur.com/eAI0rkz.png",
+    name: "Solana",
+    ticker: "SOL",
+    price: "32.83",
+    changePercentage: "-12.32",
+    TVL: "60000",
+    popularPairs: [
+      {
+        name: "Solana",
+        ticker: "SOL",
+        icon: "https://imgur.com/eAI0rkz.png",
+      },
+      {
+        name: "Ethereum",
+        ticker: "ETH",
+        icon: "https://imgur.com/Su3We8P.png",
+      },
+      {
+        name: "Binance USD",
+        ticker: "BUSD",
+        icon: "https://imgur.com/LCEltEU.png",
+      },
+    ],
+  },
+  {
+    icon: "https://imgur.com/Su3We8P.png",
+    name: "Ethereum",
+    ticker: "ETH",
+    price: "1466.45",
+    changePercentage: "-11.93",
+    TVL: "60000",
+    popularPairs: [
+      {
+        name: "Solana",
+        ticker: "SOL",
+        icon: "https://imgur.com/eAI0rkz.png",
+      },
+      {
+        name: "Ethereum",
+        ticker: "ETH",
+        icon: "https://imgur.com/Su3We8P.png",
+      },
+      {
+        name: "Binance USD",
+        ticker: "BUSD",
+        icon: "https://imgur.com/LCEltEU.png",
+      },
+    ],
+  },
+  {
+    icon: "https://imgur.com/LCEltEU.png",
+    name: "Binance USD",
+    ticker: "BUSD",
+    price: "1.00",
+    changePercentage: "+0.26",
+    TVL: "60000",
+    popularPairs: [
+      {
+        name: "Solana",
+        ticker: "SOL",
+        icon: "https://imgur.com/eAI0rkz.png",
+      },
+      {
+        name: "Ethereum",
+        ticker: "ETH",
+        icon: "https://imgur.com/Su3We8P.png",
+      },
+      {
+        name: "Binance USD",
+        ticker: "BUSD",
+        icon: "https://imgur.com/LCEltEU.png",
+      },
+    ],
+  },
+  {
+    icon: "https://imgur.com/uoQP0Sv.png",
+    name: "Shiba Inu",
+    ticker: "SHIB",
+    price: "0.00000001948",
+    changePercentage: "-8.1",
+    TVL: "60000",
+    popularPairs: [
+      {
+        name: "Solana",
+        ticker: "SOL",
+        icon: "https://imgur.com/eAI0rkz.png",
+      },
+      {
+        name: "Ethereum",
+        ticker: "ETH",
+        icon: "https://imgur.com/Su3We8P.png",
+      },
+      {
+        name: "Binance USD",
+        ticker: "BUSD",
+        icon: "https://imgur.com/LCEltEU.png",
+      },
+    ],
+  },
+];
