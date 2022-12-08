67068037e27629daa56da2fef44509227cc1fbce
krishna-saini <krishnapraanjal10@gmail.com>
2022-12-08 21:24:02 +0530

    initial commit in NamePrice component

---
+++ public/Component/NamePrice.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface NamePriceProps {
+    name:string;
+    price: number;
+    tvl:number;
+ }
+
+function NamePrice(props:NamePriceProps) {\
+    const {name, price, tvl} = props;
+  return (
+    <></>
+  )
+}
+
+export default NamePrice
