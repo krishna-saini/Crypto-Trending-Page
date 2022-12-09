d1e99f1bd30c1846eafe5ee903547d1fa443ffd7
Aditya Kadali <adityakadali00@gmail.com>
2022-12-09 06:54:52 +0000

    Added alt tag & Formatting

--- src/Component/PairCoin.tsx
+++ src/Component/PairCoin.tsx
@@ -1,21 +1,26 @@
 // import Cryptoprops from './props/props_data'
 export interface PairCoinsProps {
-    popularPairs: {
-      name: string;
-      ticker: string;
-      icon: string;
-    }[];
-   }
+  popularPairs: {
+    name: string;
+    ticker: string;
+    icon: string;
+  }[];
+}
 
-function PairCoin(props:PairCoinsProps){
-    const {popularPairs} = props
-    return(
-        <div className="w-[241px] space-y-2 text-center">
-      <p className="font-tomorrow flex h-[40px] w-[242px] items-center justify-center rounded-[17px] border-[1px] bg-[#14172B] pl-16 font-[600]">{popularPairs.map((el,i)=>( <img key={i} src={el.icon}/>))}</p>
-       <p className="font-tomorrow text-xs font-[600] text-[#5a5f7d] opacity-90">
-    Popular Pairs
-  </p></div>
-    )
+function PairCoin(props: PairCoinsProps) {
+  const { popularPairs } = props;
+  return (
+    <div className="w-[241px] space-y-2 text-center">
+      <p className="font-tomorrow flex h-[40px] w-[242px] items-center justify-center rounded-[17px] border-[1px] bg-[#14172B] pl-16 font-[600]">
+        {popularPairs.map((el, i) => (
+          <img key={i} src={el.icon} alt={`${el.name}`} />
+        ))}
+      </p>
+      <p className="font-tomorrow text-xs font-[600] text-[#5a5f7d] opacity-90">
+        Popular Pairs
+      </p>
+    </div>
+  );
 }
 
 export default PairCoin;
